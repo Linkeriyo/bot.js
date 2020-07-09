6597e89531afa165af780bb60b39ae3264c5e542
linkeriyo <linkeriyo@gmail.com>
2020-07-09 14:07:57 +0200

    making review command

--- bot.js
+++ bot.js
@@ -12,13 +12,16 @@ client.on('message', msg => {
     if (msg.content.startsWith(prefix)) {
         let command = msg.content.slice(prefix.length);
         let args = command.split(' ');
-        
-        switch (args[0]) {
+
+        switch (args[0].toLowerCase()) {
             case 'report':
                 let username = args[1], reason = command.slice(args[0].length + args[1].length + 2);
                 let reports = require("./reports.json");
                 reports.reports.push({
-                    reportID: (reports.lastID + 1), username: username, reason: reason
+                    reportID: (reports.lastID + 1),
+                    pending: true,
+                    username: username,
+                    reason: reason
                 });
 
                 reports.lastID = reports.reports[reports.reports.length - 1].reportID;
@@ -28,6 +31,30 @@ client.on('message', msg => {
                     if (err) return console.log(err);
                     else msg.reply(`${username} reportado con ID: ${reports.lastID}`);
                 });
+                break;
+            
+            case 'review':
+                if (msg.member.hasPermission("ADMINISTRATOR")) {
+                    msg.reply('check dm')
+                    let reports = require("./reports.json");
+                    let toSend = '';
+                    let pendingReports = 0;
+                    let embeds;
+
+                    reports.reports.forEach(report => {
+                        if (report.pending) {
+                            pendingReports+=1;
+                            toSend = toSend.concat(`\`ID: ${report.reportID}name: ${report.username}\nreason: ${report.reason}\``);
+                            console.log(toSend);
+                        }
+                    });
+                    if (pendingReports >= 0) {
+                        msg.member.user.send(`${pendingReports} reportes pendientes:\n` + toSend);
+                    } else {
+                        msg.member.user.send('no hay reportes pendientes :D');
+                    }
+                }
+                break;
         }
     }
 });
